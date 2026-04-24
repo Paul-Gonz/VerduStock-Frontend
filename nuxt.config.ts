ff02d377d5cd8b0fa25c6a4c873a19ceb41c6578
Paul-Gonz <gonzalezpaul204@gmail.com>
2026-04-23 23:46:56 -0400

    fix apibase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_URL,
+      apiBase: '',
     }
   },
   devtools: { enabled: true }
